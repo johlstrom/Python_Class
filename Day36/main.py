d45088be3de8d446948c54f4a83d74d6b3f2df0f
J O <johlstrom@gmail.com>
2024-06-16 10:25:46 +0200

    Day 36 complete

---
+++ Day36/main.py
@@ -0,0 +1,82 @@
+import requests
+import os
+from twilio.rest import Client
+
+STOCK_NAME = "GME"
+COMPANY_NAME = "SS&C"
+news_api = os.environ.get('NEWS_API')
+print(news_api)
+stock_api = os.environ.get('STOCK_API')
+print(stock_api)
+account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
+print(account_sid)
+auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
+print(auth_token)
+
+STOCK_ENDPOINT = "https://www.alphavantage.co/query"
+NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+
+## STEP 1: Use https://www.alphavantage.co/documentation/#daily
+# When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+
+stock_params = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": STOCK_NAME,
+    "apikey": stock_api
+}
+
+response = requests.get(STOCK_ENDPOINT, params=stock_params)
+response.raise_for_status()
+data = response.json()["Time Series (Daily)"]
+data_list = [value for (key, value) in data.items()]
+yesterday_data = data_list[0]
+yesterday_closing_price = yesterday_data["4. close"]
+print(yesterday_closing_price)
+
+day_before_yesterday_data = data_list[1]
+day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
+print(day_before_yesterday_closing_price)
+
+difference = abs(float(yesterday_closing_price) - float(day_before_yesterday_closing_price))
+print(difference)
+
+diff_percent = (difference / float(yesterday_closing_price)) * 100
+print(diff_percent)
+
+
+if diff_percent > 1:
+    news_params = {
+        "qInTitle": COMPANY_NAME,
+        "apiKey": news_api
+    }
+    
+    news_response = requests.get(NEWS_ENDPOINT, params=news_params)
+    news_response.raise_for_status()
+    articles = news_response.json()["articles"]
+
+
+#TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
+three_articles = articles[:3]
+print(three_articles)
+
+
+    ## STEP 3: Use twilio.com/docs/sms/quickstart/python
+    #to send a separate message with each article's title and description to your phone number. 
+
+#TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
+
+#TODO 9. - Send each article as a separate message via Twilio. 
+
+
+
+# #Optional TODO: Format the message like this: 
+# """
+# TSLA: 🔺2%
+# Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
+# Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
+# or
+# "TSLA: 🔻5%
+# Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
+# Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
+# """
+
